750c89ff3a9b3eca068122890b7b994d72ef7514
Unknown <abbyimac@gmail.com>
2017-09-19 16:19:03 -0600

    Number guessing game build

--- Guesser/Assets/Guessing.cs
+++ Guesser/Assets/Guessing.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Guessing : MonoBehaviour 
 {
+	public Text textBox;
+
 	private int max = 100;
 	private int min = 1;
 	private int guess;
@@ -15,6 +18,23 @@ public class Guessing : MonoBehaviour
 	void Start () 
 	{
 		guess = Random.Range (min, max);
+
+
+		textBox.text = "Welcome to Number Guesser!"
+		+ "\nPick a number in your head. "
+		+ "\n\nThe highest number you can pick is " + max + "."
+		+ "\nThe lowest number you can pick is " + min + "."
+		+ "\n I have " + counter + " chances to guess."
+		+ "\n\nIs the number higher or lower than " + guess + "?"
+		+ "\nUp arrow for higher, Down arrow for lower, Enter for equal";
+
+
+
+
+
+
+
+
 		print ("Welcome to Number Guesser");
 		print ("Pick a number in your head");
 
@@ -22,7 +42,7 @@ public class Guessing : MonoBehaviour
 		print ("The lowest number you can pick is " + min);
 
 		print ("Is the number higher or lower than " + guess + "?");
-		print ("Up arrow for higher, Down for lower, Enter for equal");
+		print ("Up arrow for higher, Down arrow for lower, Enter for equal");
 		max = max + 1;
 	}
 	
@@ -34,6 +54,7 @@ public class Guessing : MonoBehaviour
 			if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)) 
 			{
 				print ("You win!");
+				textBox.text = "<color=green>You win!</color>";
 			}
 		}	
 		else if (Input.GetKeyDown (KeyCode.UpArrow)) 
@@ -41,6 +62,7 @@ public class Guessing : MonoBehaviour
 			min = guess; 
 			guess = (max + min) / 2;
 			counter--; 
+			textBox.text = "Is the number higher or lower than " + guess + "?";
 			print ("Is the number higher or lower than " + guess + "?");
 		} 
 		else if (Input.GetKeyDown (KeyCode.DownArrow)) 
@@ -48,10 +70,12 @@ public class Guessing : MonoBehaviour
 			max = guess; 
 			guess = (max + min) / 2;
 			counter--; 
+			textBox.text = "Is the number higher or lower than " + guess + "?";
 			print ("Is the number higher or lower than " + guess + "?");
 		} 
 		if (Input.GetKeyDown (KeyCode.Return)) 
 		{
+			textBox.text = "<color=red>I win! Your number is " + guess + "!</color>";
 			print ("I win! Your number is " + guess);
 		}
 
