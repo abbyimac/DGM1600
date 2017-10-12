920f7481b72aaef12850e3518137326a4d9c9f6d
Unknown <abbyimac@gmail.com>
2017-10-12 10:04:33 -0600

    block breaker audio, pokeball, background, levels

---
+++ BlockBreaker/Assets/Scripts/FloorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour {
+
+	public LevelManager myLevelManager;
+
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		myLevelManager.LevelLoad ("GameOver");
+
+	}
+
+
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- BlockBreaker/Assets/Scripts/LevelManager.cs
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	public void ExitGame (){
+
+
+
+
+	public void LevelLoad (string name) {
+		SceneManager.LoadScene (name);
+	
+	
+	}
+
+
+
+
+
+	public void ExitGame () {
 
 		print ("Tried to Exit.");
 		Application.Quit ();
 	
 	}
+
+	
 
 }
