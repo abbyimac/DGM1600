84d1afa0b1b8ee8a7f3135fb9e54812445eb57db
Unknown <abbyimac@gmail.com>
2017-11-28 11:23:33 -0700

    Prelim Space game stuff
    last step taken was to start making explosion effect

---
+++ Space/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+	public int health;
+	public GameObject explosionEffect;
+
+
+	public void IncrementHealth (int damage){
+		health += damage;
+		if (health <= 0) {
+			Destroy (gameObject);
+			Instantiate (explosionEffect, transform.position, Quaternion.identity);
+		}
+	}
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
