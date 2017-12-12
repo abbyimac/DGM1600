276753099000258ef912527fcab0a89a58f00931
Unknown <abbyimac@gmail.com>
2017-12-11 17:42:58 -0700

    Started new space game. Finished Menu scene

---
+++ MySpaceGame/Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManagerScript : MonoBehaviour {
+
+
+	public static int meteorCount;
+
+
+
+
+	void Start () {
+		meteorCount = FindObjectsOfType<MeteorScript> ().Length;
+		print (meteorCount);
+	}
+
+	public void LevelLoad (string name) {
+		SceneManager.LoadScene (name);
+	
+	
+	}
+
+
+
+
+
+	public void ExitGame () {
+
+		print ("Tried to Exit.");
+		Application.Quit ();
+	
+	}
+
+	public void LoadNextLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+
+
+
+	}
+		
+	public void CheckBrickCount () {
+		if (meteorCount <= 0) {
+			LoadNextLevel ();
+		}
+	}
+
+}
