f3aa8da0e7e397f31646f233aeeb1b41c5944481
Unknown <abbyimac@gmail.com>
2017-12-11 21:13:05 -0700

    started meteor work

--- MySpaceGame/Assets/Scripts/MeteorScript.cs
+++ MySpaceGame/Assets/Scripts/MeteorScript.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class MeteorScript : MonoBehaviour {
 
+	private Rigidbody2D rigid;
+	public float speed = 1f;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		rigid = GetComponent<Rigidbody2D> ();
+		rigid.velocity = transform.right * speed;
+		rigid.AddTorque (Random.Range(-5f, 5f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
---
+++ MySpaceGame/Assets/Scripts/SpawnControllerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnControllerScript : MonoBehaviour {
+
+	public float spawnTime = 5f;
+	private float nextSpawn = 3f;
+	public float decay = 0.95f;
+	public GameObject meteorPrefab;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (nextSpawn > 0)
+			nextSpawn = nextSpawn - Time.deltaTime;
+		else {
+			nextSpawn = nextSpawn + spawnTime;
+			spawnTime = spawnTime * decay;
+			Instantiate (meteorPrefab);
+		}
+
+		
+	}
+}
