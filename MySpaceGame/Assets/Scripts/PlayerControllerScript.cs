e5ebdbb858898c7e297b740144bedf43952c9c05
Unknown <abbyimac@gmail.com>
2017-12-11 18:52:19 -0700

    implemented screen wrap script and player movement controls

---
+++ MySpaceGame/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+	private Rigidbody2D rigid;
+	public float thrust = 3f; 
+	public float rotationPower = 3f;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (0, 0, -Input.GetAxis ("Horizontal") * rotationPower);
+		rigid.AddForce (transform.up * Input.GetAxis ("Vertical") * thrust);
+		
+	}
+}
---
+++ MySpaceGame/Assets/Scripts/ScreenWrapScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrapScript : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		Vector3 pos = transform.position;
+		if (transform.position.y < -4.5f) 
+			pos.y = 4.5f;
+		
+		if (transform.position.y > 4.5f) 
+			pos.y = -4.5f;
+		
+		if (transform.position.x < -5.5f) 
+			pos.x = 5.5f;
+	
+		if (transform.position.x > 5.5f) 
+			pos.x = -5.5f;
+	
+		transform.position = pos;
+
+}
+
+	}
+
